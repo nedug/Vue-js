caf2512ea398ca2fa6ecf30530e47d549d0d436d
Alexander R <80103497+nedug@users.noreply.github.com>
2022-11-30 15:32:18 +0300

    44

--- src/layout/AuthLayout.vue
+++ src/layout/AuthLayout.vue
@@ -2,7 +2,7 @@
 
   <div class="container">
     <div class="card">
-      
+
     </div>
   </div>
 
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,5 +1,21 @@
 <template>
 
+  <form class="card" @submit.prevent>
+    <h1>Войти в систему</h1>
+
+    <div class="form-control">
+      <label for="email">Email</label>
+      <input type="email" id="email">
+    </div>
+
+    <div class="form-control">
+      <label for="password">Пароль</label>
+      <input type="password" id="password">
+    </div>
+
+    <button class="btn primary" type="submit">Войти</button>
+  </form>
+
 </template>
 
 <script>
