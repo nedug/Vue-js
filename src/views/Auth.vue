416608163482cf02b6aceb341aaa85a788d6ed0d
Alexander R <80103497+nedug@users.noreply.github.com>
2022-11-30 16:06:52 +0300

    Update Auth.vue

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -3,13 +3,14 @@
   <form class="card" @submit.prevent>
     <h1>Войти в систему</h1>
 
-    <div class="form-control">
+    <div :class="['form-control', {invalid: eError}]">
+      <!-- Первый класс из массива всегда, второй только при наличии ошибки -->
       <label for="email">Email</label>
       <input type="email" id="email" v-model="email"> <!-- Связываем v-model c нашей валидацией -->
       <small v-if="eError">{{ eError }}</small> <!-- Выводим ошибку из валидации -->
     </div>
 
-    <div class="form-control">
+    <div :class="['form-control', {invalid: pError}]">
       <label for="password">Пароль</label>
       <input type="password" id="password" v-model="password"> <!-- Связываем v-model c нашей валидацией -->
       <small v-if="pError">{{ pError }}</small> <!-- Выводим ошибку из валидации -->
@@ -21,13 +22,16 @@
 </template>
 
 <script>
+import * as yup from 'yup';
 import { useField } from 'vee-validate';
 
 
 export default {
   setup() {
 
-    const { value: email, errorMessage: eError, handleBlur: eBlur } = useField('email'); // Валидация из пакета 'vee-validate'
+    const { value: email, errorMessage: eError, handleBlur: eBlur } = useField(
+        'email',
+    ); // Валидация из пакета 'vee-validate'
 
     const { value: password, errorMessage: pError, handleBlur: pBlur } = useField('password');
 
