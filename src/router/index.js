b5254f25844f3727f63cf8d17b63a9ec5c35e7fd
Alexander R <80103497+nedug@users.noreply.github.com>
2022-11-30 14:30:49 +0300

    11

--- src/router/index.js
+++ src/router/index.js
@@ -9,9 +9,9 @@ const routes = [
         component: Home,
     },
     {
-        path: '/about',
-        name: 'About',
-        component: () => import('../views/About.vue'),
+        path: '/help',
+        name: 'Help',
+        component: () => import('../views/Help.vue'),
     },
 ];
 
