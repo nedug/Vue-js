4ec804684cfcb3dbb14fe47f80aaf3e30ccd8a62
Alexander R <80103497+nedug@users.noreply.github.com>
2022-11-30 16:30:19 +0300

    Update Auth.vue

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,19 +1,18 @@
 <template>
 
-  <form class="card" @submit.prevent>
+  <form class="card" @submit.prevent="onSubmit">
     <h1>Войти в систему</h1>
 
-    <div :class="['form-control', {invalid: eError}]">
-      <!-- Первый класс из массива всегда, второй только при наличии ошибки -->
+    <div :class="['form-control', {invalid: eError}]"> <!-- Первый класс из массива всегда, второй только при наличии ошибки -->
       <label for="email">Email</label>
-      <input type="email" id="email" v-model="email"> <!-- Связываем v-model c нашей валидацией -->
+      <input type="email" id="email" v-model="email" @blur="eBlur"> <!-- Связываем v-model c нашей валидацией, Слушаем событие blur -->
       <small v-if="eError">{{ eError }}</small> <!-- Выводим ошибку из валидации -->
     </div>
 
     <div :class="['form-control', {invalid: pError}]">
       <label for="password">Пароль</label>
-      <input type="password" id="password" v-model="password"> <!-- Связываем v-model c нашей валидацией -->
-      <small v-if="pError">{{ pError }}</small> <!-- Выводим ошибку из валидации -->
+      <input type="password" id="password" v-model="password" @blur="pBlur">
+      <small v-if="pError">{{ pError }}</small>
     </div>
 
     <button class="btn primary" type="submit">Войти</button>
@@ -23,20 +22,36 @@
 
 <script>
 import * as yup from 'yup';
-import { useField } from 'vee-validate';
+import { useField, useForm } from 'vee-validate';
 
 
 export default {
   setup() {
 
-    const { value: email, errorMessage: eError, handleBlur: eBlur } = useField(
-        'email',
-    ); // Валидация из пакета 'vee-validate'
+    const { handleSubmit, isSubmitting } = useForm(); // Валидация всей формы из пакета 'vee-validate'
 
-    const { value: password, errorMessage: pError, handleBlur: pBlur } = useField('password');
+    const { value: email, errorMessage: eError, handleBlur: eBlur } = useField( // Валидация полей из пакета 'vee-validate'
+        'email',
+        yup
+            .string()
+            .trim()
+            .required()
+            .email(),
+    );
+
+    const { value: password, errorMessage: pError, handleBlur: pBlur } = useField(
+        'password',
+        yup
+            .string()
+            .trim()
+            .required()
+            .min(6),
+    );
+
+    const onSubmit = handleSubmit(values => console.log(values));
 
     return {
-      email, password, eError, pError, eBlur, pBlur,
+      email, password, eError, pError, eBlur, pBlur, onSubmit,
     };
   },
 
