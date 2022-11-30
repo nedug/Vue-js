8dba2a2df0431d882e99b9e9888f5b6ac7e67a9d
Alexander R <80103497+nedug@users.noreply.github.com>
2022-11-30 17:06:11 +0300

    Update Auth.vue

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -30,38 +30,7 @@ import { useField, useForm } from 'vee-validate';
 export default {
   setup() {
 
-    const { handleSubmit, isSubmitting, submitCount } = useForm(); // Валидация всей формы из пакета 'vee-validate'
-
-    const onSubmit = handleSubmit(values => console.log(values));
-
-    const isTooManyAttempts = computed(() => submitCount.value >= 3);
-    // computed - используем, если нужно что то высчитать на основании других данных (переменных)
-
-    watch(isTooManyAttempts, value => value && setTimeout(() => submitCount.value = 0, 2000))
-    // watch - следим за какими то данной или переменной, и при изменении вызываем колбэк
-
-    const { value: email, errorMessage: eError, handleBlur: eBlur } = useField( // Валидация полей из пакета 'vee-validate'
-        'email',
-        yup
-            .string()
-            .trim()
-            .required('Введите ваш email')
-            .email('Введите корректный email'),
-    );
-    const { value: password, errorMessage: pError, handleBlur: pBlur } = useField(
-        'password',
-        yup
-            .string()
-            .trim()
-            .required('Введите ваш password')
-            .min(6, 'Пароль должен быть больше 6 символов'),
-    );
-
-    return {
-      email, password, eError, pError, eBlur, pBlur, onSubmit, isSubmitting, isTooManyAttempts,
-    };
   },
-
 };
 </script>
 
