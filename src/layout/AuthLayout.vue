8b6c515193ab2840cf9883b7fcb1b338853fbd91
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-01 15:51:50 +0300

    Update AuthLayout.vue

--- src/layout/AuthLayout.vue
+++ src/layout/AuthLayout.vue
@@ -1,13 +1,19 @@
 <template>
 
   <div class="container">
+    <app-message></app-message>
     <router-view />  <!-- Обозначем место, куда вставится наш компонент из папки Views -->
   </div>
 
 </template>
 
 <script>
-export default {};
+import AppMessage from '@/components/ui/AppMessage';
+
+
+export default {
+  components: { AppMessage },
+};
 </script>
 
 <style scoped>
