79f66a8f362520c093e83e8ae12dec7ee33218be
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-01 16:26:41 +0300

    Update AppMessage.vue

--- src/components/ui/AppMessage.vue
+++ src/components/ui/AppMessage.vue
@@ -1,17 +1,10 @@
 <template>
+
   <div v-if="message" :class="['alert', message.type]">
-    <!--      <p class="alert-title" v-if="title">{{title}}</p>-->
+    <p class="alert-title" v-if="title">{{ title }}</p>
     <p>{{ message.value }}</p>
     <span class="alert-close" @click="close">&times;</span>
   </div>
-
-  <!--    <div v-if="message" :class="['alert', 'danger']">-->
-
-  <!--      <p>{{message}}</p>-->
-  <!--&lt;!&ndash;      <span class="alert-close" @click="close">&times;</span>&ndash;&gt;-->
-  <!--    </div>-->
-
-  <!--  <div class="card">Привет!!</div>-->
 
 </template>
 
@@ -25,9 +18,20 @@ export default {
     const store = useStore(); // Получаем store из Vuex
     const message = computed(() => store.state.message);
 
-    return { message };
-  },
+    const TITLE_MAP = {
+      primary: 'Успешно!',
+      danger: 'Ошибка!',
+      warning: 'Внимание!',
+    };
 
+    const title = computed(() => message.value ? TITLE_MAP[message.value.type] : '');
+
+    const close = () => {
+      store.commit('clearMessage');
+    };
+
+    return { message, title, close };
+  },
 };
 </script>
 
