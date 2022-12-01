587b105a3e7095c2edaed0fea27f30557bcaeae0
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-01 15:48:27 +0300

    Create AooMessage.vue

---
+++ src/components/ui/AooMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="message" :class="['alert', message.type]">
+    <p class="alert-title" v-if="title">{{title}}</p>
+    <p>{{message.value}}</p>
+    <span class="alert-close" @click="close">&times;</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+</style>
