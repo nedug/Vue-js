160f2638a613a75543690114654582c5a3603868
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-01 12:16:49 +0300

    Update auth.module.js

--- src/store/modules/auth.module.js
+++ src/store/modules/auth.module.js
@@ -1,8 +1,18 @@
 export default {
     namespaced: true, // чтобы actions были локальными, а не глобальными
-    state() {
+
+    state() { // Формируем State
         return {
             token: null,
-        }
-    }
-}
+        };
+    },
+
+    getters: { // Для получения State без его изменени
+        token(state) {
+            return state.token;
+        },
+        isAuth(state) {
+            return !!state.token;
+        },
+    },
+};
