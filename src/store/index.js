8dcc4e8678a32d08e449448173e5bff0feb4c72e
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-01 12:39:54 +0300

    11

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,11 @@ import { createStore } from 'vuex';
 
 
 export default createStore({
-    state: {},
-    mutations: {},
+    state: {}, // Формируем глобальный State
+
+    mutations: {}, // Для мутации Store
+
     actions: {},
+
     modules: {}, // Разделение Store на отдельные модули
 });
