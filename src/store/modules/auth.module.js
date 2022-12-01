a1e46c90544655c4c3d20a364a0177dc56075351
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-01 13:01:47 +0300

    Update auth.module.js

--- src/store/modules/auth.module.js
+++ src/store/modules/auth.module.js
@@ -11,13 +11,23 @@ export default {
 // В функции-обработчике мы выполняем фактические модификации состояния, и она получает ЛОКАЛЬНОЕ состояние в качестве первого аргумента.
 // Функции обработчика мутаций должны быть синхронными!!!
 // Вызов мутаций через commit
+        setToken(state, token) {
+            state.token = token;
+            localStorage.setItem('jwt-token', token);
+        },
+        logout(state) {
+            state.token = null;
+            localStorage.removeItem('jwt-token');
+        },
 
     },
 
     actions: { // Actions аналогичны мутациям, разница в том, что: actions меняют State через мутации и actions могут содержать асинхронные операции.
-// Обработчики Actions получают объект Context (context.commit, context.state, context.getters, context.dispatch)
+// Actions получают объект Context (context.commit, context.state, context.getters, context.dispatch)
 // Actions запускаются - store.dispatch('increment')
-
+        async login({ commit }) {
+            commit('setToken', 'TEST TOKEN')
+        },
     },
 
     getters: { // Для получения State без его изменения (Геттеры получат ЛОКАЛЬНОЕ состояние в качестве своего первого аргумента)
