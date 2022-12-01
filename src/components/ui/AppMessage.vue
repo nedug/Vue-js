ffc48ce66be2c3f68b4c9734eec82ef5e40e646f
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-01 15:51:59 +0300

    22

---
+++ src/components/ui/AppMessage.vue
@@ -0,0 +1,20 @@
+<template>
+<!--  <div v-if="message" :class="['alert', message.type]">-->
+<!--    <p class="alert-title" v-if="title">{{title}}</p>-->
+<!--    <p>{{message.value}}</p>-->
+<!--    <span class="alert-close" @click="close">&times;</span>-->
+<!--  </div>-->
+
+  <div class="card">Привет!!</div>
+
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+</style>
