e9ffcae33a1aa73f5def25f93f3d9871dde559e8
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-01 14:51:31 +0300

    Update error.js

---
+++ src/utils/error.js
@@ -0,0 +1,7 @@
+const ERROR_CODES = {
+    EMAIL_NOT_FOUND: 'Пользователь с таким email не был найден',
+    INVALID_PASSWORD: 'Пароль неверный',
+    auth: 'Пожалуйста войдите в систему',
+};
+
+export const error = code => ERROR_CODES[code] ? ERROR_CODES[code] : 'Неизвестная ошибка';
