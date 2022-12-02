58b952bfac8e562b0eb1f0b1dcc1755ddec04610
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-02 14:50:31 +0300

    Update request.js

--- src/axios/request.js
+++ src/axios/request.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
+import router from '../router';
 
 
 export const requestAxios = axios.create({
     baseURL: process.env.VUE_APP_FB_URL,
+});
+
+// Если ТОКЕН просрочен и ответ сервера - ошибка сервера 401 (нет авторизации). Тогда перехватываем ответ и перенаправляем на страницу авторизации
+requestAxios.interceptors.response.use(null, error => { // Для перехвата ответа сервера. Проверяем при ошибке
+    if (error.response.status === 401) {
+        router.push('/auth?message=auth');
+    }
+
+    return Promise.reject(error);
 });
