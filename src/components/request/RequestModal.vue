999519a5b438b4b00406be7cd0491be64f0ca2c1
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-02 10:59:38 +0300

    Update RequestModal.vue

--- src/components/request/RequestModal.vue
+++ src/components/request/RequestModal.vue
@@ -14,7 +14,7 @@
       </div>
 
       <div class="form-control" :class="{invalid: aError}">
-         <label for="amount">Суммы</label>
+         <label for="amount">Сумма</label>
          <input type="number" id="amount" v-model.number="amount" @blur="aBlur">
          <small v-if="aError">{{ aError }}</small>
       </div>
@@ -22,9 +22,9 @@
       <div class="form-control">
          <label for="status">Статус</label>
          <select id="status" v-model="status">
+            <option value="active">Активен</option>
             <option value="done">Завершен</option>
             <option value="cancelled">Отменен</option>
-            <option value="active">Активен</option>
             <option value="pending">Выполняется</option>
          </select>
       </div>
