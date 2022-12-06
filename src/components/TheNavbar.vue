73e0e6410834d2e772e598b43bb3eaa07a637b24
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-06 12:02:04 +0300

    Update TheNavbar.vue

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,23 +1,23 @@
 <template>
 
-  <nav class="navbar">
-    <h1>Online Bank</h1>
+   <nav class="navbar">
+      <h1>Online Bank</h1>
 
-    <ul class="navbar-menu">
-      <li>
-        <router-link to="/">Заявки</router-link>
-      </li>
-      <li>
-        <router-link to="/help">Помощь</router-link>
-      </li>
-      <li>
-        <a href="#">Сообщения</a>
-      </li>
-      <li>
-        <a href="#" @click.prevent="logout">Выход</a>
-      </li>
-    </ul>
-  </nav>
+      <ul class="navbar-menu">
+         <li>
+            <router-link to="/">Заявки</router-link>
+         </li>
+         <li>
+            <router-link to="/help">Помощь</router-link>
+         </li>
+         <li>
+            <a href="#">Сообщения</a>
+         </li>
+         <li>
+            <a href="#" @click.prevent="logout">Выход</a>
+         </li>
+      </ul>
+   </nav>
 
 </template>
 
@@ -27,17 +27,17 @@ import { useRouter } from 'vue-router';
 
 
 export default {
-  setup() {
-    const store = useStore(); // Получаем store из Vuex
-    const router = useRouter(); // Получаем router из vue-router
+   setup() {
+      const store = useStore(); // Получаем store из Vuex
+      const router = useRouter(); // Получаем router из vue-router
 
-    return {
-      logout: () => {
-        store.commit('auth/logout'); // Запускаем мутацию logout
-        router.push('/auth'); // Перенапаправляем на страницу HOME
-      },
-    };
-  },
+      return {
+         logout: () => {
+            store.commit('auth/logout'); // Запускаем мутацию logout
+            router.push('/auth'); // Перенапаправляем на страницу HOME
+         },
+      };
+   },
 
 };
 </script>
