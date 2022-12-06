ac035da0b0a17eb0778b4e252f63ccebfa2370b4
Alexander R <80103497+nedug@users.noreply.github.com>
2022-12-06 14:18:09 +0300

    Updates

---
+++ precache-manifest.a38404c84b900f67ba2fd58ec89a7cb4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36dd14146a2111d40953",
+    "url": "/css/app.5f98ff28.css"
+  },
+  {
+    "revision": "2a50115b50c928b846d52bbfc4bee2b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36dd14146a2111d40953",
+    "url": "/js/app.80d6d811.js"
+  },
+  {
+    "revision": "0dfe61cceea92093e583",
+    "url": "/js/chunk-2d0a4783.5e059fc5.js"
+  },
+  {
+    "revision": "819c05a7efd3a0e42b42",
+    "url": "/js/chunk-2d0be70b.bb8de04e.js"
+  },
+  {
+    "revision": "5b5cd6d3bff6cb9daf40",
+    "url": "/js/chunk-2d216dc7.5a9fb8be.js"
+  },
+  {
+    "revision": "2025b0a51ca23387f570",
+    "url": "/js/chunk-vendors.5e35dbe6.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
